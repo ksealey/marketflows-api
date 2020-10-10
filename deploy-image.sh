2c47bc1c8d8542fc11aac397e7107687c34b4f8e
Khareem Sealey <ksealey@Khareems-MacBook-Pro.local>
2020-10-10 00:14:51 -0400

    Updated deploy image script

--- deploy-image.sh
+++ deploy-image.sh
@@ -7,11 +7,6 @@ if [ -z "$1" ]
 fi
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $PROD_REPO_URL
-if [ $? -eq 0 ]
-    then
-        exit
-fi
-
 docker build -t marketflows-api . --no-cache
 docker tag marketflows-api:$1 $PROD_REPO_URL/marketflows-api:$1
 docker push $PROD_REPO_URL/marketflows-api:$1
